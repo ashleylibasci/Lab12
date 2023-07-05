fb0ac767d93555a4d266ecbc492a2fbf780e53bf
Your Name <youremail@yourdomain.com>
2023-07-05 13:34:33 -0500

    update

--- src/Lab12.ino
+++ src/Lab12.ino
@@ -6,12 +6,38 @@ SYSTEM_THREAD(ENABLED);
 
 OledWingAdafruit display;
 
+#include "MQTT.h"
+
+void callback(char *topic, byte *payload, unsigned int length);
+
+MQTT client("lab.thewcl.com", 1883, callback);
+
 void setup()
 {
   Serial.begin(9600);
-  Blynk.begin(auth);
+  Blynk.begin(AUTH);
+
+  display.setup();
+  display.clearDisplay();
+  display.display();
 }
 
 void loop()
 {
+  Blynk.run();
+  display.loop();
+
+  if (client.isConnected())
+  {
+    client.loop();
+  }
+  else
+  {
+    client.connect(System.deviceID());
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  // write my code here
 }
