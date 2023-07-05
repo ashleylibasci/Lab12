dca1cf860f7181a6222a6c1600965c015d3dde5c
Your Name <youremail@yourdomain.com>
2023-07-05 13:00:52 -0500

    update

---
+++ src/Lab12.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/ashleylibasci/IoTFolderLabs/Lab12/src/Lab12.ino"
+/*
+ * Project Lab12
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/ashleylibasci/IoTFolderLabs/Lab12/src/Lab12.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
